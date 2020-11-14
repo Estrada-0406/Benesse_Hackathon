00dcab3badc7e51cf69e155c7351ed64bcc8a091
YutoNakashima <74189491+Estrada-0406@users.noreply.github.com>
2020-11-15 00:50:22 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-//テスト
+import 'package:tcard/tcard.dart';
+import 'package:newsapi/newsapi.dart';
+
+
 class Person{
   String firstName;
   String lastName;
@@ -38,7 +41,7 @@ class Choice extends StatelessWidget {
       child:Row(
           children: <Widget>[
             Expanded(
-              child : FlatButton(
+              child : RaisedButton(
                   onPressed: (){
                     Navigator.push(
                         context,
@@ -46,14 +49,24 @@ class Choice extends StatelessWidget {
                         )
                     );
                   },
+                  highlightElevation: 16,
+                  highlightColor: Colors.blue,
+                  onHighlightChanged: (value) {},
                   child: Text('勉強面')
               ),
             ),
             Expanded(
-              child : FlatButton(
+              child : RaisedButton(
                   onPressed: (){
-                    print('bbb');
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => NextPage2(),
+                        )
+                    );
                   },
+                  highlightElevation: 16,
+                  highlightColor: Colors.blue,
+                  onHighlightChanged: (value) {},
                   child: Text('キャリア面')
               ),
             ),
@@ -69,36 +82,126 @@ class NextPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: Text("次のページ"),
-    ),
-    body: Column(
-    children:<Widget>[
-      Expanded(
-        child : FlatButton(
-            onPressed: (){
-              print('国語');
-            },
-            child: Text('国語')
+          title: Text("次のページ"),
+        ),
+        body: Column(
+          children:<Widget>[
+            Expanded(
+              child : RaisedButton(
+
+                  onPressed: (){
+                    print('国語');
+                  },
+                  child: Text('国語')
+              ),
+            ),
+            Expanded(
+              child : RaisedButton(
+                  onPressed: (){
+                    print('数学');
+                  },
+                  child: Text('数学')
+              ),
+            ),
+            Expanded(
+              child : RaisedButton(
+                  onPressed: (){
+                    print('理科');
+                  },
+                  child: Text('理科')
+              ),
+            ),
+          ],
+        )
+    );
+  }
+}
+
+
+//News画面の導入
+class NextPage2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(
+        title:Text("キャリア面"),
+      ),
+      body:Column(
+          children:<Widget>[
+            Expanded(
+                child:Text("次のページでは、気になるトピックとそうでないトピックを選んでね！"
+                    "「次へ」ボタンで次に進もう！")
+            ),
+            Expanded(
+              child : RaisedButton(
+                  onPressed: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => NewsScreen(),
+                        )
+                    );
+                  },
+                  child: Text('次へ')
+              ),
+            ),
+          ]
+      ),
+    );
+  }
+}
+
+
+//News画面の表示
+const apiKey = '3cf1d14308bd4105a2486f2155a49bfa';
+
+class NewsScreen extends StatefulWidget {
+  @override
+  _NewsScreenState createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getNewsData();
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  void getNewsData() async {
+    var newsApi = NewsApi();
+    newsApi.init(
+      debugLog: true,
+      apiKey: 'foo',
+    );
+
+    newsApi.apiKey = apiKey;
+
+    ArticleResponse topHeadlines = await newsApi.topHeadlines(
+      country: 'jp',
+    );
+    List<Object> a = topHeadlines.props;
+
+    var x = topHeadlines.toJson();
+
+    //print(topHeadlines.toJson());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TCard(
+          cards: [
+            Container(color: Colors.blue),
+            Container(color: Colors.yellow),
+            Container(color: Colors.red),
+          ],
         ),
       ),
-      Expanded(
-        child : FlatButton(
-            onPressed: (){
-              print('数学');
-            },
-            child: Text('数学')
-        ),
-      ),
-      Expanded(
-        child : FlatButton(
-            onPressed: (){
-              print('理科');
-            },
-            child: Text('理科')
-        ),
-      ),
-    ],
-    )
     );
   }
 }
